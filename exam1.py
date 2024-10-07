a055a82edb619c8f280eb2f09866c014999ef9cc
ahreum <ahahreum19990@naver.com>
2024-10-08 03:12:51 +0900

    Initial commit

---
+++ exam1.py
@@ -0,0 +1,10 @@
+name = input('이름을 입력하세요 : ')
+print('%s님, 환영합니다.' %name)
+print('-' * 20)
+
+age = int(input('나이를 입력하세요 : '))
+print('당신의 나이는 %d살 이군요.' %age)
+print('-' * 20)
+
+height = float(input('키를 입력하세요 : '))
+print('당신의 키는 %.1f 이군요' %height)
---
+++ exam2.py
@@ -0,0 +1,11 @@
+num1 = int(input('첫 번째 정수를 입력하세요: '))
+num2 = int(input('두 번째 정수를 입력하세요: '))
+result = num1 + num2
+print('%d + %d = %d' %(num1, num2, result))
+print('-' * 20)
+
+num1 = input('첫 번째 정수를 입력하세요: ')
+num2 = input('두 번째 정수를 입력하세요: ')
+result = int(num1) + int(num2)
+print('%s + %s = %s' %(num1, num2, result))
+print('-' * 20)
